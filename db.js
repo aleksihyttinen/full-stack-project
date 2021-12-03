7071f86cc5905d1998dc93f6f0f3cdad6af5c07e
aleksihyttinen <72377162+aleksihyttinen@users.noreply.github.com>
2021-12-03 19:52:14 +0200

    Add files via upload

---
+++ db.js
@@ -0,0 +1,151 @@
+const mysql = require("mysql");
+require("dotenv").config();
+const Validator = require("jsonschema").Validator;
+const validator = new Validator();
+
+const schema = {
+  id: "location",
+  type: "object",
+  properties: {
+    id: {
+      type: "number",
+      minimum: 0,
+    },
+    latitude: {
+      type: "number",
+      minimum: -90,
+      maximum: 90,
+    },
+    longitude: {
+      type: "number",
+      minimum: -180,
+      maximum: 180,
+    },
+  },
+  additionalProperties: false,
+};
+
+const connection = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DB,
+});
+
+let connectionFunctions = {
+  connect: () => {
+    if (err) throw err;
+    connection.getConnection();
+  },
+  close: () => {
+    return new Promise((resolve, reject) => {
+      connection.end((err) => reject(err));
+      resolve("Connection closed");
+    });
+  },
+  save: (location) => {
+    const validLocation = validator.validate(location, schema);
+    return new Promise((resolve, reject) => {
+      if (validLocation.errors.length == 0) {
+        connection.query(
+          "INSERT INTO locations (latitude, longitude) VALUES (?, ?)",
+          [location.latitude, location.longitude],
+          (err, result) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(result.insertId);
+          }
+        );
+      } else {
+        reject(validLocation.errors);
+      }
+    });
+  },
+
+  findAll: (queries) => {
+    return new Promise((resolve, reject) => {
+      if (Object.keys(queries).length === 0) {
+        connection.query("SELECT * FROM locations", (err, locations) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(locations);
+        });
+      } else if (queries.minLat && queries.maxLat) {
+        connection.query(
+          "SELECT * FROM locations WHERE latitude > ? AND latitude < ?",
+          [queries.minLat, queries.maxLat],
+          (err, locations) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(locations);
+          }
+        );
+      } else if (queries.minLon && queries.maxLon) {
+        connection.query(
+          "SELECT * FROM locations WHERE longitude > ? AND longitude < ?",
+          [queries.minLon, queries.maxLon],
+          (err, locations) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(locations);
+          }
+        );
+      } else if (queries.sortBy) {
+        connection.query(
+          "SELECT * FROM locations ORDER BY " + queries.sortBy,
+          (err, locations) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(locations);
+          }
+        );
+      } else {
+        reject("Invalid queries/parameters");
+      }
+    });
+  },
+  deleteById: (id) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        "DELETE FROM locations WHERE id=?",
+        id,
+        (err, result) => {
+          if (err) {
+            reject(err);
+          }
+          if (result.affectedRows == 0) {
+            reject("Id not found");
+          } else {
+            resolve(`Deleted id: ${id} successfully`);
+          }
+        }
+      );
+    });
+  },
+  findById: (id) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        "SELECT * FROM locations WHERE id=?",
+        id,
+        (err, location) => {
+          if (err) {
+            reject(err);
+          }
+          if (location.length == 0) {
+            reject("Id not found");
+          } else {
+            resolve(location);
+          }
+        }
+      );
+    });
+  },
+};
+
+module.exports = connectionFunctions;
---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+const connection = require("./db.js");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("frontend/build"));
+
+app.get("/locations", async (req, res) => {
+  try {
+    let data = await connection.findAll(req.query);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.statusCode = 400;
+    res.end();
+  }
+});
+
+app.get("/locations/:id([0-9]+)", async (req, res) => {
+  let id = req.params.id;
+  try {
+    let temp = await connection.findById(id);
+    if (temp) {
+      res.send(temp);
+    }
+  } catch (err) {
+    console.log(err);
+    if (err == "Id not found") {
+      res.statusCode = 404;
+      res.end();
+    }
+  }
+});
+
+app.delete("/locations/:id([0-9]+)", async (req, res) => {
+  let id = req.params.id;
+  try {
+    let result = await connection.deleteById(id);
+    res.send(result);
+    res.statusCode = 204;
+    res.end();
+  } catch (err) {
+    console.log(err);
+    if (err == "Id not found") {
+      res.statusCode = 404;
+      res.end();
+    }
+  }
+});
+
+app.post("/locations", async (req, res) => {
+  let location = req.body;
+  try {
+    let addedLocation = await connection.save(location);
+    res.statusCode = 201;
+    console.log({
+      id: addedLocation,
+      latitude: location.latitude,
+      longitude: location.longitude,
+    });
+    res.send({
+      id: addedLocation,
+      latitude: location.latitude,
+      longitude: location.longitude,
+    });
+  } catch (err) {
+    console.log(err);
+    res.statusCode = 400;
+    res.end();
+  }
+});
+
+const server = app.listen(8080, () => {
+  console.log(`Listening on port ${server.address().port}`);
+});
