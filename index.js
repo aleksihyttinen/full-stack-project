251160ae8d8ace00bc30a4cf3f9f85477278beae
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2021-12-03 14:34:04 +0200

    Add config and get

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 const port = process.env.PORT || 8080;
+
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
 app.use(cors());
 
-const db = [{ name: "tiina" }, { name: "jack" }];
-
-app.get("/names", (req, res) => {
-  res.send(db);
+var pool = mysql.createPool(config);
+app.get("/", (req, res) => {
+  pool.query("SELECT * from location", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
 });
 
 const server = app.listen(port, () => {
